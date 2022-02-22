6537b72df57f6f82ae8f1faf3b84d83296a1953d
sebas <sebastianvagodirioo@yahoo.com.ar>
2022-02-22 10:46:48 -0300

    first version stable

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+/*import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;*/
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.bind.annotation.GetMapping;
-
+//@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
 @SpringBootTest
 class DemoApplicationTests {
 
