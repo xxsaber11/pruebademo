94325b664ef87dfe4e16e342506fe1539d86df80
sebas <sebastianvagodirioo@yahoo.com.ar>
2022-02-22 09:57:29 -0300

    fix error references @getmapping

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@GetMapping
+	public String hello() {
+		return "Hello World";
 	}
 
 }
