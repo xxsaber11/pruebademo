f936749b6bddb6175200940a440bd69122b9f1e4
sebas <sebastianvagodirioo@yahoo.com.ar>
2022-03-08 10:07:58 -0300

    Se añadio Carpeta Student y Clase Student. se arma primer usuario

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +19,16 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hello() {
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Mariam",
+						"mariam.jamal@gmail.com",
+						LocalDate.of(2000, Month.JANUARY, 5),
+						21
 
-		return "Hello World";
+				)
+		);
 	}
 }
