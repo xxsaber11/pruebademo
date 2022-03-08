1d260818e616c8f1b649cad8c47fce5be9dfc65b
sebas <sebastianvagodirioo@yahoo.com.ar>
2022-03-08 10:22:35 -0300

    the access path changed by api/v1/student in localhost:8080. version stable

--- src/main/java/com/example/demo/Student/StudentController.java
+++ src/main/java/com/example/demo/Student/StudentController.java
@@ -1,2 +1,28 @@
-package com.example.demo.Student;public class StudentController {
+package com.example.demo.Student;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path =  "api/v1/student")
+public class StudentController {
+    @GetMapping
+    public List<Student> GetStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+
+                )
+        );
+    }
 }
